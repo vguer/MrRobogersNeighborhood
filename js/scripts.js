57eeb9d31521aa4699867d389a33ecb79827aec7
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-12 11:20:10 -0700

    Will add business logic that is broken at time of commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,31 @@ $(document).ready(function() {
   $('form#neighborhood').submit(function(event) {
     event.preventDefault();
 
-    const userInput = numberToRoboger($("input#inputnumber").val());
+    const userInput = numberToRoboger($("input#numberinput").val());
 
-    $('#output').number(userInput);
+    $("form").hide();
+    $('#output').append(numberToRoboger(userInput));
   });
 });
 
 
 // Buisness Logic
+
+const inputNumbers = number.split("");
+const robogerWords = ["", "Beep", "Boop", "Won't you be my neighbor?"];
+
+function numberToRoboger (inputNumbers) {
+  if (number > 100) {
+    return "no"
+  } else if (inputNumbers === 1) {
+    return robogerWords[1]; 
+  }
+}
+
+
+
+
+ 
+  
+  }
+}
