655ea3a7fcc1e68126e6b0cb5d4af377c17c4ab4
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-12 10:47:40 -0700

    Commence javascript UI Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+// UI Logic
+
 $(document).ready(function() {
+  $('form#neighborhood').submit(function(event) {
+    event.preventDefault();
 
-  event.preventDefault();
+    const userInput = numberToRoboger($("input#inputnumber").val());
 
+    $('#output').number(userInput);
+  });
 });
-});
+
+
+// Buisness Logic
