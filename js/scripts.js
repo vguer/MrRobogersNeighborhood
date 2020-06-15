9788f334dd2e4781d81374b4032c694fbd488472
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-14 21:53:55 -0700

    Add loop to business logic that is not currently showing at output

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,13 @@ $(document).ready(function() {
     const numberInput = parseInt($("input#numberinput").val());
     console.log(numberInput)
     const result = numberToRoboger(numberInput);
+    const result2 = rangeCreator(numberInput);
     console.log(result)
-    // $("form").hide();
 
 
-    $(".output").append(result);
+    $(".output").append(result, result2);
     $("#result").show();
+    
   
   });
 });
@@ -33,27 +34,9 @@ function numberToRoboger(numberInput) {
   }
 }
 
-
-
-
-/*
-const userInput = ('#numberinput');
-const inputNumbers = (userInput)
-const robogerWords = ['', 'Beep', 'Boop', "Won't you be my neighbor?"];
-
-function numberToRoboger (userInput) {
-for (let robogerWords = 0; robogerWords <= robogerWords.length; robogerWords +=1)
-  if (userInput === 1) {
-    return robogerWords [1];
-  } else return "no" 
-  };
-  console.log(userInput);
-
-
-
-  if (inputNumbers > 100) {
-    return "no"
-  } else if (inputNumbers === 1) {
-    return robogerWords[1]
-  } console.log(inputNumbers)
-*/
+function rangeCreator(numberInput) {
+  let finalArray =[];
+  for (let index=0; index <= numberInput-1; index+=1){
+    finalArray.push(index++);
+}
+}
